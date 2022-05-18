e66237d419c89c5e6b06ff319a23d97c25642dc9
rmcstt <anisio.nanato@hotmail.com>
2022-05-18 15:45:47 -0300

    finished

--- map1.js
+++ map1.js
@@ -5,3 +5,11 @@ let resultados = nums.map(function (e) {
 })
 
 console.log(resultados, nums) ///[ 2, 4, 6, 8, 10 ]   [ 1, 2, 3, 4, 5 ]
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const praDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultados = nums.map(soma10).map(triplo).map(praDinheiro)
+
+console.log(resultados) ///[ 'R$ 33,00', 'R$ 36,00', 'R$ 39,00', 'R$ 42,00', 'R$ 45,00' ]
